76ebe31ce3f7a3c3e7ddf1a826dfb722c4a86d5d
chandanabhat16 <chandana16bhat@gmail.com>
2024-09-20 02:03:13 -0400

    changes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,12 +1,12 @@
-import React, { useState,useEffect } from 'react';
-import './ProductList.css'
-import CartItem from './CartItem';
-import { useDispatch } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { addItem } from './CartSlice';
+import CartItem from './CartItem';
+import './ProductList.css';
+
 function ProductList() {
-    const [showCart, setShowCart] = useState(false); 
-    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-    const [addedToCart, setAddedToCart] = useState({});
+    const [showCart, setShowCart] = useState(false);
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -215,95 +215,76 @@ function ProductList() {
             ]
         }
     ];
-    const dispatch = useDispatch();
+
     const handleAddToCart = (product) => {
-        dispatch(addItem(product));
-        setAddedToCart((prevState) => ({
-           ...prevState,
-           [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+        dispatch(addItem({
+            name: product.name,
+            image: product.image,
+            cost: product.cost.replace('$', ''), // Optional: convert cost to a number if needed
         }));
     };
-   const styleObj={
-    backgroundColor: '#4CAF50',
-    color: '#fff!important',
-    padding: '15px',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignIems: 'center',
-    fontSize: '20px',
-   }
-   const styleObjUl={
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '1100px',
-   }
-   const styleA={
-    color: 'white',
-    fontSize: '30px',
-    textDecoration: 'none',
-   }
-   const cartItems = useSelector(state => state.cart.items);
-   const totalQuantity = cartItems.reduce((total, item) => total + item.quantity, 0);
-   const handleCartClick = (e) => {
-    e.preventDefault();
-    setShowCart(true); // Set showCart to true when cart icon is clicked
-};
-const handlePlantsClick = (e) => {
-    e.preventDefault();
-    setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
-    setShowCart(false); // Hide the cart when navigating to About Us
-};
 
-   const handleContinueShopping = (e) => {
-    e.preventDefault();
-    setShowCart(false);
-  };
-  return (
-    <div>
-        <div className="navbar" style={{ backgroundColor: '#4CAF50', color: '#fff', padding: '15px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <div className="tag">
-                <h3>Paradise Nursery</h3>
-                <i>Where Green Meets Serenity</i>
-            </div>
-            <div>
-                <a href="#" onClick={handleCartClick} style={{ color: 'white', fontSize: '30px', textDecoration: 'none' }}>
-                    <h1 className='cart'>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" height="68" width="68">
-                            <circle cx="80" cy="216" r="12"></circle>
-                            <circle cx="184" cy="216" r="12"></circle>
-                            <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
-                        </svg>
-                        {totalQuantity > 0 && <span className="cart-quantity">{totalQuantity}</span>}
-                    </h1>
-                </a>
-            </div>
-        </div>
+    // Get cart items from the Redux store
+    const cartItems = useSelector(state => state.cart.items);
 
-        {!showCart ? (
-            <div className="product-grid">
-                {plantsArray.map((category, index) => (
-                    <div key={index}>
-                        <h1>{category.category}</h1>
-                        <div className="product-list">
-                            {category.plants.map((plant, plantIndex) => (
-                                <div className="product-card" key={plantIndex}>
-                                    <img className="product-image" src={plant.image} alt={plant.name} />
-                                    <div className="product-title">{plant.name}</div>
-                                    <div className="product-description">{plant.description}</div>
-                                    <div className="product-cost">Cost: {plant.cost}</div>
-                                    <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
-                                </div>
-                            ))}
+    // Calculate total quantity of items in the cart
+    const totalQuantity = cartItems.reduce((total, item) => total + item.quantity, 0);
+
+    const handleCartClick = (e) => {
+        e.preventDefault();
+        setShowCart(true);
+    };
+
+    const handleContinueShopping = (e) => {
+        e.preventDefault();
+        setShowCart(false);
+    };
+
+    return (
+        <div>
+            <div className="navbar" style={{ backgroundColor: '#4CAF50', color: '#fff', padding: '15px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <div className="tag">
+                    <h3>Paradise Nursery</h3>
+                    <i>Where Green Meets Serenity</i>
+                </div>
+                <div>
+                    <a href="#" onClick={handleCartClick} style={{ color: 'white', fontSize: '30px', textDecoration: 'none' }}>
+                        <h1 className='cart'>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" height="68" width="68">
+                                <circle cx="80" cy="216" r="12"></circle>
+                                <circle cx="184" cy="216" r="12"></circle>
+                                <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+                            </svg>
+                            {totalQuantity > 0 && <span className="cart-quantity">{totalQuantity}</span>}
+                        </h1>
+                    </a>
+                </div>
+            </div>
+
+            {!showCart ? (
+                <div className="product-grid">
+                    {plantsArray.map((category, index) => (
+                        <div key={index}>
+                            <h1>{category.category}</h1>
+                            <div className="product-list">
+                                {category.plants.map((plant, plantIndex) => (
+                                    <div className="product-card" key={plantIndex}>
+                                        <img className="product-image" src={plant.image} alt={plant.name} />
+                                        <div className="product-title">{plant.name}</div>
+                                        <div className="product-description">{plant.description}</div>
+                                        <div className="product-cost">Cost: {plant.cost}</div>
+                                        <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
-                    </div>
-                ))}
-            </div>
-        ) : (
-            <CartItem onContinueShopping={handleContinueShopping} />
-        )}
-    </div>
- );
+                    ))}
+                </div>
+            ) : (
+                <CartItem onContinueShopping={handleContinueShopping} />
+            )}
+        </div>
+    );
 }
 
 export default ProductList;
