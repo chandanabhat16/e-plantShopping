dbfa14cf749b6b9875535e14c97682f60a5e4753
chandanabhat16 <chandana16bhat@gmail.com>
2024-09-20 02:11:31 -0400

    changes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -230,6 +230,26 @@ function ProductList() {
     // Calculate total quantity of items in the cart
     const totalQuantity = cartItems.reduce((total, item) => total + item.quantity, 0);
 
+    const styleObj={
+        backgroundColor: '#4CAF50',
+        color: '#fff!important',
+        padding: '15px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignIems: 'center',
+        fontSize: '20px',
+    }
+       const styleObjUl={
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '1100px',
+    }
+       const styleA={
+        color: 'white',
+        fontSize: '30px',
+        textDecoration: 'none',
+    }
     const handleCartClick = (e) => {
         e.preventDefault();
         setShowCart(true);
